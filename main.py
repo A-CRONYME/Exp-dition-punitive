9513361ec01fcfea2bb05961cca36bd834b16643
Florent Beaudrix <florent.beaudrix@proton.me>
2024-11-25 18:32:15 +0100

    feat: added the core

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from expedition_punitive.scheduler import schedule_email_randomized
 def main():
     print("Bienvenue dans l'application Expédition Punitive.")
     
-    # Configurer les informations de l'email
+    # Configurer les informations de l'email et le fichier joint
     to_email = "destinataire@example.com"
     subject = "Message posthume"
     body = "Ceci est un message posthume. Prenez soin de vous."
     attachment_path = "chemin/vers/votre_fichier.pdf"
 
-    # Spécifier la plage de délais en secondes (par exemple, entre 30 et 120 secondes)
+    # Spécifier la plage de délais en secondes (simple test en secondes, a modifier pour des semaines, mois, etc.)
     min_delay = 30
     max_delay = 120
 
-    # Planifier l'envoi aléatoire
+    # Planifier l'envoi aléatoire de l'email avec le fichier joint
     schedule_email_randomized(to_email, subject, body, attachment_path, min_delay, max_delay)
 
 if __name__ == "__main__":
